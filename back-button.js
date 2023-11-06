e7a8a055f3a30132f69e05f1121e5e7cfdd9acf9
Gaurav Bharat <gaurav.mendse@gmail.com>
2023-11-06 10:56:29 +0530

    fixed back button

--- back-button.js
+++ back-button.js
@@ -5,7 +5,7 @@ anchor.innerHTML = "&larr;";
 anchor.setAttribute("class", "text-blue-500 hover:text-blue-300");
 div.setAttribute(
   "class",
-  "absolute top-0 right-0 bg-slate-300 px-2 py-1 rounded-lg m-2 shadow-lg"
+  "fixed top-0 right-0 bg-slate-300 px-2 py-1 rounded-lg m-2 shadow-lg"
 );
 div.appendChild(anchor);
 document.body.appendChild(div);
